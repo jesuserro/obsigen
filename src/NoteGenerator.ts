a0647216fc54755c50a53b3e6e85c466402472e8
olagato <olagato@gmail.com>
2023-06-11 17:23:29 +0200

    chore: commit save point

--- src/NoteGenerator.ts
+++ src/NoteGenerator.ts
@@ -1,9 +1,6 @@
-import * as ReactDOMServer from 'react-dom/server';
-
-
 import { App, TFile } from 'obsidian';
+import * as ReactDOMServer from 'react-dom/server';
 import { YamlTemplate } from './templates/YamlTemplate.jsx';
-
 
 export class NoteGenerator {
   app: App;
@@ -16,24 +13,12 @@ export class NoteGenerator {
     const timestamp = Date.now();
     const newFileName = `Note_${timestamp}.md`;
 
-
-    // Render the YamlTemplate component to a string
     const yamlContent = ReactDOMServer.renderToStaticMarkup(
       YamlTemplate({ data })
     );
-    // const yamlContent2 = yaml.dump(yamlContent);
-    
-    // const yamlData = {
-    //   title: 'My Note',
-    //   aliases: ['alias1', 'alias2'],
-    //   date: new Date().toISOString(),
-    // };
-    // const yamlContent2 = yaml.dump(yamlData, { indent: 2 });
-
 
     const templateContent = `# ${data.title}\nAa`;
 
-    // Combine the YAML content and template content
     const fileData = `${yamlContent}\n${templateContent}`;
 
     const newFile: TFile = await this.app.vault.create(newFileName, fileData);
