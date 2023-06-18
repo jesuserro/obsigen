0a72e40e6a702f282c5c70c8299b800f256c5efa
olagato <olagato@gmail.com>
2023-06-18 10:01:46 +0200

    chore: commit save point

--- src/templates/Yaml.tsx
+++ src/templates/Yaml.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { iYaml } from '../interface/Yaml';
 
-export function Yaml({ data }: { data: iYaml }) {
+export function Yaml( data: iYaml ) {
   return (
     <>
       ---{"\n"}
