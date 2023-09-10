a76249e0158c97ec10970f007bd86c887bc3f34d
olagato <olagato@gmail.com>
2023-09-10 10:02:27 +0200

    refactor(Month): Added Month view BETA.

--- src/core/notes/calendar/CalendarMonth.tsx
+++ src/core/notes/calendar/CalendarMonth.tsx
@@ -1,64 +1,58 @@
 import React from 'react';
 
-// Function to create the calendar grid
-function createDaysGrid() {
-  const daysGrid = [];
+function CalendarMonth() {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+  const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+  const numDaysInMonth = lastDayOfMonth.getDate();
+  const firstDayOfWeek = firstDayOfMonth.getDay(); // 0 for Sunday, 1 for Monday, etc.
 
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth();
-  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  const firstDayOfWeek = new Date(currentYear, currentMonth, 1).getDay();
+  // Calculate the difference in starting days between Sunday (0) and Monday (1)
+  const dayOffset = firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1;
 
   // Calculate the number of rows needed for the calendar
-  const numRows = Math.ceil((daysInMonth + firstDayOfWeek) / 7);
+  const numRows = Math.ceil((numDaysInMonth + dayOffset) / 7);
 
-  for (let row = 0; row < numRows; row++) {
-    const cells = [];
+  const createDaysGrid = () => {
+    const daysGrid = [];
 
-    for (let day = 0; day < 7; day++) {
-      const dayIndex = row * 7 + day + 1 - firstDayOfWeek;
+    for (let row = 0; row < numRows; row++) {
+      const cells = [];
 
-      if (dayIndex >= 1 && dayIndex <= daysInMonth) {
-        // Create a cell for each day in the month
-        cells.push(<td key={dayIndex}>{dayIndex}</td>);
-      } else {
-        // Create empty cells for days outside the current month
-        cells.push(<td key={`empty-${row}-${day}`}></td>);
+      for (let dayOfWeek = 0; dayOfWeek < 7; dayOfWeek++) {
+        const dayIndex = row * 7 + dayOfWeek + 1 - dayOffset;
+        const isWithinMonth = dayIndex >= 1 && dayIndex <= numDaysInMonth;
+
+        cells.push(
+          <td key={dayOfWeek} className={isWithinMonth ? 'within-month' : 'outside-month'}>
+            {isWithinMonth ? dayIndex : ''}
+          </td>
+        );
       }
+
+      daysGrid.push(<tr key={row}>{cells}</tr>);
     }
 
-    // Create a row with the cells
-    daysGrid.push(<tr key={row}>{cells}</tr>);
-  }
+    return <tbody>{daysGrid}</tbody>;
+  };
 
-  return (
-      <tbody>{daysGrid}</tbody>
-  );
-}
-
-function CalendarMonth() {
   return (
     <table className="calendar-table">
       <thead>
         <tr>
-          <th>Sun</th>
           <th>Mon</th>
           <th>Tue</th>
           <th>Wed</th>
           <th>Thu</th>
           <th>Fri</th>
           <th>Sat</th>
+          <th>Sun</th>
         </tr>
       </thead>
-      <tbody>{createDaysGrid()}</tbody>
+      {createDaysGrid()}
     </table>
   );
 }
 
 export default CalendarMonth;
-
-
-
-
-
