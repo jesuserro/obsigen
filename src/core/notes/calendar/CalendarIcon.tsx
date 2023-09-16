90f9a067776fcb3e20747cc6c1ea25161e3af1e2
olagato <olagato@gmail.com>
2023-09-16 21:01:38 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarIcon.tsx
+++ src/core/notes/calendar/CalendarIcon.tsx
@@ -1,21 +1,27 @@
 import { TFile } from 'obsidian';
 import React from 'react';
 import { AiFillHeart } from 'react-icons/ai';
-import { FaCalendarDay, FaQuestionCircle } from 'react-icons/fa';
+import { FaLink, FaQuestionCircle, FaRegClock } from 'react-icons/fa';
 import { GiChurch } from 'react-icons/gi';
 
 export class CalendarIcon {
   static getIcon(note: TFile): React.ReactNode {
     const path = note.path;
-    // console.log(path);
+
+    const size = 14;
+
     if (path.includes('Anna') || path.includes("Nieves")) {
-      return <AiFillHeart size={12} style={{ color: '#FF69B4' }} />;
-    } else if (path.match(/\/(\d{8})\.md$/)) {
-      return <FaCalendarDay size={12} style={{ color: '#8B4513' }} />;
+      return <AiFillHeart size={size} style={{ color: '#FF69B4' }} />;
     } else if (path.includes('TO -')) {
-      return <GiChurch size={12} style={{ color: '#800080' }} />;
+      return <GiChurch size={size} style={{ color: '#800080' }} />;
+    } else if (path.includes('/Moments')) {
+      // Handle Moments with a sand watch icon and yellow color
+      return <FaRegClock size={size} style={{ color: '#FFFF99' }} />;
+    } else if (path.includes('/Captures')) {
+      // Handle Captures with a URL link icon and sky blue color
+      return <FaLink size={size} style={{ color: '#87CEEB' }} />;
     } else {
-      return <FaQuestionCircle size={12} style={{ color: '#A9A9A9' }} />;
+      return <FaQuestionCircle size={size} style={{ color: '#A9A9A9' }} />;
     }
   }
 }
