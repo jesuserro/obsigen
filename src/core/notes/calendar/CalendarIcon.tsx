5490886511818f48a1e32544c2ff2233d5caaabf
olagato <olagato@gmail.com>
2023-09-17 11:11:10 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarIcon.tsx
+++ src/core/notes/calendar/CalendarIcon.tsx
@@ -1,20 +1,23 @@
 import { App, TFile } from 'obsidian';
 import React from 'react';
 import { AiFillHeart } from 'react-icons/ai';
-import { FaBirthdayCake, FaGlobe, FaQuestionCircle, FaSun } from 'react-icons/fa'; // Import the FaSun icon for weather
-import { GiChurch, GiSandsOfTime } from 'react-icons/gi';
+import { FaBirthdayCake, FaGlobe, FaQuestionCircle } from 'react-icons/fa'; // Import the FaSun icon for weather
+import { GiChurch, GiSandsOfTime, GiThermometerCold } from 'react-icons/gi';
 
 export class CalendarIcon {
   static getIcon(note: TFile, app: App): React.ReactNode {
     const path = note.path;
+    const size = 14;
 
     const tags = app.metadataCache.getFileCache(note)?.frontmatter?.tags;
-    const size = 14;
+    if (tags) {
+      if(tags.includes('weather')){
+        return <GiThermometerCold size={size} style={{ color: '#6dc8f2' }} />; 
+      }
+    }
 
     if (path.includes('Anna') || path.includes("Nieves")) {
       return <AiFillHeart size={size} style={{ color: '#FF69B4' }} />;
-    } else if (tags && tags.includes('weather')) {
-      return <FaSun size={size} style={{ color: '#FFD700' }} />; 
     } else if (path.includes('TO -')) {
       return <GiChurch size={size} style={{ color: '#ad6df2' }} />;
     } else if (path.includes('/Aniversaries')) {
