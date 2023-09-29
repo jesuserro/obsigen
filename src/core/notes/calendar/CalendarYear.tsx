05025b8b91665e0e55dcbb8543528a406bdb6d2e
olagato <olagato@gmail.com>
2023-09-29 20:12:17 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarYear.tsx
+++ src/core/notes/calendar/CalendarYear.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import CalendarMonth from './CalendarMonth';
 
-const CalendarYear: React.FC = (): React.JSX.Element => {
+function CalendarYear(): JSX.Element {
   const currentYear = new Date().getFullYear();
 
   const monthsGrid = Array.from({ length: 12 }, (_, month) => (
     <CalendarMonth key={String(month).padStart(2, '0')} year={currentYear} month={month} />
   ));
 
-  return <div>{monthsGrid}</div>;
+  return (
+    <>
+      <div>{monthsGrid}</div>
+    </>
+  );
+
 }
 
 export default CalendarYear;
