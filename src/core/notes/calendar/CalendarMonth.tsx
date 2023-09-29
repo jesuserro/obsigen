df172d0a9818af8f4c2525e3652d286715ffdb30
olagato <olagato@gmail.com>
2023-09-29 20:44:19 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarMonth.tsx
+++ src/core/notes/calendar/CalendarMonth.tsx
@@ -93,23 +93,25 @@ function CalendarMonth({year, month}: CalendarMonthProps): JSX.Element {
   monthNameAndYear = monthNameAndYear.charAt(0).toUpperCase() + monthNameAndYear.slice(1);
 
   return (
-    <div>
-      <h2>{monthNameAndYear}</h2>
-      <table className="calendar-table">
-        <thead>
-          <tr>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-            <th>Sun</th>
-          </tr>
-        </thead>
-        <tbody>{daysGrid}</tbody>
-      </table>
-    </div>
+    <>
+      <div>
+        <h2>{monthNameAndYear}</h2>
+        <table className="calendar-table">
+          <thead>
+            <tr>
+              <th>Mon</th>
+              <th>Tue</th>
+              <th>Wed</th>
+              <th>Thu</th>
+              <th>Fri</th>
+              <th>Sat</th>
+              <th>Sun</th>
+            </tr>
+          </thead>
+          <tbody>{daysGrid}</tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
