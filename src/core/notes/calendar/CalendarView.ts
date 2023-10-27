6e95f6077b35899594aa08cf54f0edc78bb4fd08
olagato <olagato@gmail.com>
2023-10-27 12:50:38 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarView.ts
+++ src/core/notes/calendar/CalendarView.ts
@@ -1,80 +1,70 @@
 import { ItemView, WorkspaceLeaf } from 'obsidian';
-export const CALENDAR_VIEW_TYPE = 'obsigen-calendar-view';
-export const CONTAINER_ID = "obsigen-calendar-container";
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AppContext } from './../../shared/appContext';
 import { CalendarEvent, FormValues } from './CalendarEvent';
 import CalendarYear from './CalendarYear';
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AppContext } from './../../shared/appContext';
 import YearSelect from './YearSelect';
 
+export const CALENDAR_VIEW_TYPE = 'obsigen-calendar-view';
+export const CONTAINER_ID = 'obsigen-calendar-container';
+
 export class CalendarView extends ItemView {
-    private reactComponent: React.ReactElement;
-    private today: Date;
-    private currentYear: number;
-    private selectElement: HTMLSelectElement; 
-    private root: ReactDOM.Root;
+  private reactComponent: React.ReactElement;
+  private root: ReactDOM.Root;
+  private currentYear: number;
+  private today: Date;
 
-    constructor(leaf: WorkspaceLeaf) {
-        super(leaf);
-        this.today = new Date();
-        this.currentYear = this.today.getFullYear();
-        // Inicializa this.root con createRoot en el contenedor adecuado
-        this.root = ReactDOM.createRoot(this.contentEl as HTMLElement);
-    }
+  constructor(leaf: WorkspaceLeaf) {
+    super(leaf);
+    this.today = new Date();
+    this.currentYear = this.today.getFullYear();
+    this.root = ReactDOM.createRoot(this.contentEl as HTMLElement);
+  }
 
-    getViewType(): string {
-        return CALENDAR_VIEW_TYPE;
-    }
+  private handleAddEvent = async () => {
+    await new CalendarEvent(this.app, this.today.getFullYear(), this.today.getMonth() + 1, this.today.getDate())
+      .openModal()
+      .then((values: FormValues) => {
+        // Manejar los valores del evento si es necesario
+        // console.log(values);
+      })
+      .catch((error) => {
+        // Manejar el error si es necesario
+        console.error(error);
+      });
+  };
 
-    getDisplayText(): string {
-        return 'Month Calendar';
-    }
+  private handleYearChange = (selectedYear: number) => {
+    this.currentYear = selectedYear; // Actualizar el año seleccionado
+    this.renderComponent();
+  };
 
-    getIcon(): string {
-        return 'calendar-days';
-    }
+  private renderComponent() {
+    const button = React.createElement('button', { onClick: this.handleAddEvent }, 'Add Event');
+    this.reactComponent = React.createElement(
+      AppContext.Provider,
+      { value: this.app },
+      button,
+      React.createElement('div', null, React.createElement(YearSelect, { currentYear: this.currentYear, onChange: this.handleYearChange })),
+      React.createElement(CalendarYear, { year: this.currentYear })
+    );
+    this.root.render(this.reactComponent);
+  }
 
-    async onOpen() {
-        
-        const handleAddEvent = async () => {
-            await new CalendarEvent(this.app, this.today.getFullYear(), this.today.getMonth() + 1, this.today.getDate()).openModal()
-            .then((values: FormValues) => {
-                // Manejar los valores del evento si es necesario
-                // console.log(values);
-            })
-            .catch((error) => {
-                // Manejar el error si es necesario
-                console.error(error);
-            });
-        };
-        const button = React.createElement('button', { onClick: handleAddEvent }, 'Add Event');
-        
+  getViewType(): string {
+    return CALENDAR_VIEW_TYPE;
+  }
 
-        const handleYearChange = (selectedYear: number) => {
-            this.currentYear = selectedYear; // Actualizar el año seleccionado
-            
-            this.reactComponent = React.createElement(
-                AppContext.Provider, { value: this.app }, 
-                button, 
-                React.createElement('div', null, React.createElement(YearSelect, { currentYear: this.currentYear, onChange: handleYearChange })),
-                React.createElement(CalendarYear, { year: this.currentYear })
-            );
-            // Renderizar el contenido principal utilizando this.root.render
-            this.root.render(this.reactComponent);
-        };
-    
-        // Modificar la salida global en el createRoot
-        this.reactComponent = React.createElement(
-            AppContext.Provider, { value: this.app }, 
-            button, 
-            React.createElement('div', null, React.createElement(YearSelect, { currentYear: this.currentYear, onChange: handleYearChange })),
-            React.createElement(CalendarYear, { year: this.currentYear })
-        );
-        // Renderizar el contenido principal utilizando this.root.render
-        this.root.render(this.reactComponent);
-    }
+  getDisplayText(): string {
+    return 'Month Calendar';
+  }
 
+  getIcon(): string {
+    return 'calendar-days';
+  }
+
+  async onOpen() {
+    this.renderComponent();
+  }
 }
