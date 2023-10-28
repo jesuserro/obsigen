888a50800595dc83fe04b6af9d8127ae7a45a5c8
olagato <olagato@gmail.com>
2023-10-28 12:31:08 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarYear.tsx
+++ src/core/notes/calendar/CalendarYear.tsx
@@ -6,9 +6,12 @@ interface CalendarYearProps {
 
 function CalendarYear({ year }: CalendarYearProps): JSX.Element {
   
-  const monthsGrid = Array.from({ length: 12 }, (_, month) => (
-    <CalendarMonth key={String(month).padStart(2, '0')} year={year} month={month} />
-  ));
+  const monthsGrid = Array.from({ length: 12 }, (_, month) => {
+    month = month + 1;
+    return (
+      <CalendarMonth key={String(month).padStart(2, '0')} year={year} month={month} />
+    );
+  });
 
   return (
     <>
