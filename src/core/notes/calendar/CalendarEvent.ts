9ceb5a31ce567631e3a0c3d631172ad87988ef67
olagato <olagato@gmail.com>
2023-12-02 10:38:47 +0100

    chore: commit save point

--- src/core/notes/calendar/CalendarEvent.ts
+++ src/core/notes/calendar/CalendarEvent.ts
@@ -275,7 +275,7 @@ export class CalendarEvent extends Modal {
 
     const selectedTime = `${selectedHour.padStart(2, '0')}:${selectedMinute.padStart(2, '0')}`;
     
-    const startDate = `${selectedYear}-${selectedMonth.padStart(2, '0')}-${selectedDay.padStart(2, '0')}T${selectedTime}`;
+    const startDate = `${selectedYear}-${selectedMonth.padStart(2, '0')}-${selectedDay.padStart(2, '0')} ${selectedTime}`;
 
     this.locations = this.locationField.getValue();
     this.urls = this.urlField.getValue();
