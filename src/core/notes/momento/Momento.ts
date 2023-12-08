9b37497d2d0459ed15e8f3d0388ce410adbbb3c4
olagato <olagato@gmail.com>
2023-12-08 17:31:01 +0100

    chore: commit save point

--- src/core/notes/momento/Momento.ts
+++ src/core/notes/momento/Momento.ts
@@ -93,9 +93,10 @@ export class Momento {
       const year = date.getFullYear();
       const month = (date.getMonth() + 1).toString().padStart(2, '0');
       const day = date.getDate().toString().padStart(2, '0');
-      const hours = date.getHours();
-      const minutes = date.getMinutes();
-      return `${year}-${month}-${day}T${hours}:${minutes}:00`;
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const seconds = date.getSeconds().toString().padStart(2, '0');
+      return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
   }
 
   async createNote(title: string, content: string, startDate?: Date, icon?: string, description?: string, locations?: string, urls:string = '') {
