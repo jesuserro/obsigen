e52dbeec92364a44f155b68603cd50a01d801a22
olagato <olagato@gmail.com>
2024-03-28 19:35:26 +0100

    chore: commit save point

--- src/api/Goodreads/Review.ts
+++ src/api/Goodreads/Review.ts
@@ -1,31 +1,26 @@
 import fetch from 'node-fetch';
 import { App } from 'obsidian';
+import { MyPluginSettings } from 'src/core/shared/interface/MyPluginSettings';
 
-// Método para obtener la información de un libro desde la API de Goodreads
+// Método para obtener el feed de reviews desde la API de Goodreads
 export async function getReviews(app:App, shelf: string): Promise<String | null> {
 
-    const setting = (app as any).setting;
-    const pluginTab = setting.pluginTabs.find((tab: any) => tab.id === 'obsigen');
-    const plugin = pluginTab.plugin;
-    const GOODREADS_USER = plugin.settings.goodreads_user;
-    const GOODREADS_APIKEY = plugin.settings.goodreads_apikey;
+    const { goodreads_user, goodreads_apikey }: MyPluginSettings = (app as any).setting.pluginTabs.find((tab: any) => tab.id === 'obsigen')?.plugin?.settings ?? {};
 
-    const url = `https://www.goodreads.com/review/list_rss/${GOODREADS_USER}?key=${GOODREADS_APIKEY}&shelf=${shelf}`;
-
-    console.log("url", url);
+    const url = `https://www.goodreads.com/review/list_rss/${goodreads_user}?key=${goodreads_apikey}&shelf=${shelf}`;
     
     try {
         
         const respuesta = await fetch(url);
-        if (respuesta.ok) {
-            const respuesta = await fetch(url);
-            const textoRespuesta = await respuesta.text();
-            console.log('Texto de respuesta:', textoRespuesta); 
-            return textoRespuesta;
-        } else {
+        if (!respuesta.ok) {
             console.error(`Error al obtener la información de reviews: ${respuesta.status}`);
-            return null;
+            return null; 
         }
+
+        const textoRespuesta = await respuesta.text();
+        console.log('Texto de respuesta:', textoRespuesta); 
+        return textoRespuesta;
+
     } catch (error) {
         console.error(`Error de red al obtener la información de reviews: ${error}`);
         return null;
