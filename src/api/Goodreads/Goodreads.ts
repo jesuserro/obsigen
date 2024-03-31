4d285909edfbcfeba80d6fe41ce498d1b1fd3f2d
olagato <olagato@gmail.com>
2024-03-31 12:29:39 +0200

    chore: commit save point

--- src/api/Goodreads/Goodreads.ts
+++ src/api/Goodreads/Goodreads.ts
@@ -17,28 +17,62 @@ export class Goodreads {
     }
 
     public async getFeedReviewsByShelf(shelf: string): Promise<string | null> {
-        const { goodreads_user, goodreads_apikey }: MyPluginSettings = (this.app as any).setting.pluginTabs.find((tab: any) => tab.id === 'obsigen')?.plugin?.settings ?? {};
+        const xmlString = await this.fetchReviewsXmlString(shelf);
+        return xmlString;
+    }
 
+    public async getRandomReview() {
+        const xmlString = await this.getFeedReviewsByShelf('read');
+        if (!xmlString) return;
+
+        const reviews = await this.parseReviews(xmlString);
+        this.processRandomReview(reviews);
+    }
+
+    public async getReviewByGuid(guid: string) {
+        const xmlString = await this.getFeedReviewsByShelf('read');
+        if (!xmlString) return;
+
+        const reviews = await this.parseReviews(xmlString);
+        const review = reviews.find(review => review.guid === guid);
+        if (!review) {
+            console.error(`No se encontró ninguna revisión con el GUID: ${guid}`);
+            return;
+        }
+        this.processReview(review);
+    }
+
+    public async getReviewByIsbn(isbn: string) {
+        const xmlString = await this.getFeedReviewsByShelf('read');
+        if (!xmlString) return;
+
+        const reviews = await this.parseReviews(xmlString);
+        const review = reviews.find(review => review.isbn === isbn);
+        if (!review) {
+            console.error(`No se encontró ninguna revisión con el ISBN: ${isbn}`);
+            return;
+        }
+        this.processReview(review);
+    }
+
+    private async fetchReviewsXmlString(shelf: string): Promise<string | null> {
+        const { goodreads_user, goodreads_apikey }: MyPluginSettings = (this.app as any).setting.pluginTabs.find((tab: any) => tab.id === 'obsigen')?.plugin?.settings ?? {};
         const url = `${Goodreads.GOODREADS_URL_BASE}/${Goodreads.GOODREADS_RSS_REVIEWS_URL}/${goodreads_user}?key=${goodreads_apikey}&shelf=${shelf}`;
 
         try {
             const response = await requestUrl(url);
-            // console.log(response.text);
             return response.text;
-
         } catch (error) {
             console.error(`Error de red al obtener la información de reviews: ${error}`);
             return null;
         }
     }
 
-    public async parseReviews(xmlString: string): Promise<any[]> {
+    private async parseReviews(xmlString: string): Promise<any[]> {
         try {
             const xmlDoc = this.parser.parseFromString(xmlString, 'text/xml');
             const items = xmlDoc.querySelectorAll('item');
-
             const reviews = Array.from(items).map(item => this.parseReviewItem(item));
-
             return reviews;
         } catch (error) {
             console.error(`Error al parsear el XML: ${error}`);
@@ -50,14 +84,11 @@ export class Goodreads {
         const shelvesElement = item.querySelector('user_shelves');
         const shelves = shelvesElement ? shelvesElement.textContent?.split(',').map(shelf => shelf.trim()) : [];
 
-        const guidMatch = item.querySelector('guid')?.textContent?.match(/\d+/);
-        const guid = guidMatch ? guidMatch[0] : null;
-
         let content = item.querySelector('user_review')?.textContent ?? '';
         content = this.turndownService.turndown(content);
 
         return {
-            guid: guid,
+            guid: item.querySelector('guid')?.textContent?.match(/\d+/)?.[0] || null,
             isbn: item.querySelector('isbn')?.textContent,
             title: item.querySelector('title')?.textContent,
             authors: item.querySelector('author_name')?.textContent,
@@ -71,66 +102,17 @@ export class Goodreads {
         };
     }
 
-    public async getRandomReview() {
-        const xmlString = await this.getFeedReviewsByShelf('read');
-        if (!xmlString) return;
-
-        const reviews = await this.parseReviews(xmlString);
+    private async processRandomReview(reviews: any[]) {
         console.log(`Número total de revisiones: ${reviews.length}`);
-
         const randomIndex = Math.floor(Math.random() * reviews.length);
         const randomReview = reviews[randomIndex];
-        console.log(randomReview);
-
-        const date = new Date(randomReview.date);
-
-        new Review(date).createNote(this.app, randomReview);
-
-        console.log('Detalles de la revisión seleccionada al azar:');
-        Object.entries(randomReview).forEach(([key, value]) => {
-            console.log(`${key}: ${value}`);
-        });
+        this.processReview(randomReview);
     }
 
-    public async getReviewByGuid(guid: string) {
-        const xmlString = await this.getFeedReviewsByShelf('read');
-        if (!xmlString) return;
-
-        const reviews = await this.parseReviews(xmlString);
-        const review = reviews.find(review => review.guid === guid);
-
-        if (!review) {
-            console.error(`No se encontró ninguna revisión con el GUID: ${guid}`);
-            return;
-        }
-
+    private async processReview(review: any) {
         const date = new Date(review.date);
-
         new Review(date).createNote(this.app, review);
-
-        console.log('Detalles de la revisión seleccionada por GUID:');
-        Object.entries(review).forEach(([key, value]) => {
-            console.log(`${key}: ${value}`);
-        });
-    }
-
-    public async getReviewByIsbn(isbn: string) {
-        const xmlString = await this.getFeedReviewsByShelf('read');
-        if (!xmlString) return;
-
-        const reviews = await this.parseReviews(xmlString);
-        const review = reviews.find(review => review.isbn === isbn);
-
-        if (!review) {
-            console.error(`No se encontró ninguna revisión con el ISBN: ${isbn}`);
-            return;
-        }
-
-        const date = new Date(review.date);
-
-        new Review(date).createNote(this.app, review);
-
-        console.log('Detalles de la revisión seleccionada por ISBN:');
+        console.log('Detalles de la revisión seleccionada:');
         Object.entries(review).forEach(([key, value]) => {
             console.log(`${key}: ${value}`);
         });
