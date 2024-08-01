3343f2e35378a269866dfc08e0b99e47481e7e7f
Jesús Erro <jesus@centraldereservas.com>
2024-08-01 16:02:16 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarIconPicker.tsx
+++ src/core/notes/calendar/CalendarIconPicker.tsx
@@ -8,18 +8,18 @@ export interface CalendarIconPickerProps {
 }
 
 const iconGroups = {
-  Emojis: ['birthday', 'fatigue', 'sadness', 'happiness', 'angry', 'tired', 'sick', 'insomnia', 'anxiety', 'cough', 'fever', 'gluttony', 'inlove', 'pumpkin', 'sex', 'dance', 'party', 'woman', 'family', 'people', 'friends', 'learning', 'psychology', 'baby'],
-  Gente: ['person', 'achelm', 'annas', 'charo', 'dad', 'gonzalo', 'natalia', 'timothy', 'josefita', 'mom', 'irene', 'kote', 'luis', 'pilar', 'nieves', 'josemi', 'ramon', 'victor', 'sophie'],
-  Tiempo: ['sunny', 'storm', 'haze', 'rain', 'snow', 'weather', 'cold', 'earthquake', 'eclipse'],
-  Economía: ['barber', 'buy', 'repairs', 'bills', 'euro', 'payroll', 'coin', 'bank', 'bbva', 'finances', 'itv', 'hacienda', 'gift', 'tax', 'saving', 'lottery', 'price', 'visa'],
-  IT: ['blogpost', 'podcast', 'email', 'phone', 'writing', 'meeting', 'google', 'rss', 'lenovo', 'spotify', 'github', 'goodreads', 'amazon', 'instagram', 'linkedin', 'kindle', 'aws', 'git', 'android', 'googlemeet', 'openai', 'gpt', 'obsidian', 'whatsapp', 'tinder', 'plentyoffish', 'youtube', 'twitter', 'dell', 'plex', 'telegram', 'skype', 'readwise', 'samsung' ],
-  Agro: ['village', 'agro', 'agriculture', 'farmer', 'tree', 'dog'],
-  Deportes: ['soccer', 'basketball', 'champions', 'hiking', 'gym', 'chess', 'orejona', 'realmadrid', 'olympics', 'sport', 'pool'],
-  Banderas: ['argentina', 'france', 'israel', 'italy', 'vatican', 'usa', 'venezuela', 'romania', 'poland', 'spain', 'russia'],
-  Religión: ['arburua', 'prayer', 'confession', 'inspiration', 'miracle', 'fatima', 'christ', 'mass', 'bible', 'prophecy', 'funeral', 'priest', 'garabandal', 'gospa', 'funeralhome', 'saint', 'saintwoman', 'nun', 'catholic', 'rosary', 'adoration', 'advent', 'emaus', 'islam', 'diosidencia', 'holyspirit', 'orthodox', 'pope', 'peace', 'wedding', 'death', 'cemetery', 'evil', 'communism', 'agenda2030'],
-  Gastronomía: ['cafe', 'cook', 'beer', 'pancake', 'friedeggs', 'gastronomy'],
-  Transporte: ['car', 'train', 'bus', 'plane', 'travel'],
-  Salud: ['strong', 'healthok', 'healthko', 'heartbreak', 'dream', 'insomnia', 'legs', 'pill', 'pilloff', 'supplement', 'doctor', 'health', 'foot', 'virus'],
+  Emojis: ['angry', 'anxiety', 'baby', 'birthday', 'cough', 'dance', 'family', 'fatigue', 'fever', 'friends', 'gluttony', 'happiness', 'inlove', 'insomnia', 'learning', 'party', 'people', 'psychology', 'pumpkin', 'sadness', 'sex', 'sick', 'tired', 'woman'],
+  Gente: ['achelm', 'annas', 'charo', 'dad', 'gonzalo', 'irene', 'josefita', 'josemi', 'kote', 'luis', 'mom', 'natalia', 'nieves', 'person', 'pilar', 'ramon', 'sophie', 'timothy', 'victor'],
+  Tiempo: ['cold', 'earthquake', 'eclipse', 'haze', 'rain', 'snow', 'storm', 'sunny', 'weather'],
+  Economía: ['bank', 'barber', 'bbva', 'bills', 'buy', 'coin', 'euro', 'finances', 'gift', 'hacienda', 'itv', 'lottery', 'payroll', 'price', 'repairs', 'saving', 'tax', 'visa'],
+  IT: ['amazon', 'android', 'aws', 'blogpost', 'dell', 'email', 'git', 'github', 'google', 'googlemeet', 'goodreads', 'gpt', 'instagram', 'kindle', 'lenovo', 'linkedin', 'meeting', 'obsidian', 'openai', 'phone', 'plentyoffish', 'podcast', 'readwise', 'rss', 'samsung', 'skype', 'spotify', 'telegram', 'tinder', 'twitter', 'whatsapp', 'writing', 'youtube'],
+  Agro: ['agriculture', 'agro', 'dog', 'farmer', 'tree', 'village'],
+  Deportes: ['basketball', 'champions', 'chess', 'gym', 'hiking', 'olympics', 'orejona', 'pool', 'realmadrid', 'soccer', 'sport'],
+  Banderas: ['argentina', 'france', 'israel', 'italy', 'poland', 'romania', 'russia', 'spain', 'usa', 'vatican', 'venezuela'],
+  Religión: ['adoration', 'advent', 'agenda2030', 'arburua', 'bible', 'catholic', 'cemetery', 'christ', 'communism', 'confession', 'death', 'diosidencia', 'emaus', 'evil', 'fatima', 'funeral', 'funeralhome', 'garabandal', 'gospa', 'holyspirit', 'inspiration', 'islam', 'mass', 'miracle', 'nun', 'orthodox', 'peace', 'pope', 'prayer', 'priest', 'prophecy', 'rosary', 'saint', 'saintwoman', 'wedding'],
+  Gastronomía: ['beer', 'cafe', 'cook', 'friedeggs', 'gastronomy', 'pancake'],
+  Transporte: ['bus', 'car', 'plane', 'train', 'travel'],
+  Salud: ['doctor', 'dream', 'foot', 'health', 'healthko', 'healthok', 'heartbreak', 'insomnia', 'legs', 'pill', 'pilloff', 'strong', 'supplement', 'virus'],
   Otros: [] as string[],
 };
 
