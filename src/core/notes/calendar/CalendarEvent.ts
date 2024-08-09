3f378bb1002102df8ba357d63cde8c7758766263
Jesús Erro <jesus@centraldereservas.com>
2024-08-09 13:15:44 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarEvent.ts
+++ src/core/notes/calendar/CalendarEvent.ts
@@ -160,19 +160,19 @@ export class CalendarEvent extends Modal {
             this.eraField.addOption("DC", "DC");
             this.eraField.addOption("AC", "AC");
             this.eraField.setValue("DC");
-
+    
             // Create year input
             this.yearField = new TextComponent(div);
             this.yearField.inputEl.addClass("form-input");
             this.yearField.setPlaceholder("e.g., 0100 for 100 AC, 0005 for 5 DC");
             this.yearField.setValue(this.formatYear(this.initialDate.getFullYear()));
         });
-
-        this.monthDropdown = this.createDropdown(parent, "Month", 1, 12);
-        this.dayDropdown = this.createDropdown(parent, "Day", 1, 31);
+    
+        this.dayDropdown = this.createDropdown(parent, "Day", 1, 31); // Día después del año
+        this.monthDropdown = this.createDropdown(parent, "Month", 1, 12); // Mes después del día
         this.hourDropdown = this.createDropdown(parent, "Hour", 0, 23);
         this.minuteDropdown = this.createDropdown(parent, "Minute", 0, 55, 5);
-
+    
         this.setDefaultDateTime(this.initialDate, this.monthDropdown, this.dayDropdown, this.hourDropdown, this.minuteDropdown);
     }
 
@@ -183,22 +183,22 @@ export class CalendarEvent extends Modal {
             this.endEraField.addOption("DC", "DC");
             this.endEraField.addOption("AC", "AC");
             this.endEraField.setValue("DC");
-
+    
             // Create end year input
             this.endYearField = new TextComponent(div);
             this.endYearField.inputEl.addClass("form-input");
-            this.endYearField.setPlaceholder("e.g., write year number");
+            this.endYearField.setPlaceholder("e.g., 0100 for 100 AC, 0005 for 5 DC");
             this.endYearField.setValue(this.formatYear(this.initialDate.getFullYear()));
         });
-
-        this.endMonthDropdown = this.createDropdown(parent, "End Month", 1, 12);
-        this.endDayDropdown = this.createDropdown(parent, "End Day", 1, 31);
+    
+        this.endDayDropdown = this.createDropdown(parent, "End Day", 1, 31); // Día después del año
+        this.endMonthDropdown = this.createDropdown(parent, "End Month", 1, 12); // Mes después del día
         this.endHourDropdown = this.createDropdown(parent, "End Hour", 0, 23);
         this.endMinuteDropdown = this.createDropdown(parent, "End Minute", 0, 55, 5);
-
+    
         this.setDefaultDateTime(this.initialDate, this.endMonthDropdown, this.endDayDropdown, this.endHourDropdown, this.endMinuteDropdown);
     }
-
+    
     private createDropdown(parent: HTMLElement, label: string, start: number, end: number, step: number = 1) {
         const div = parent.createDiv("form-element");
         const dropdown = new DropdownComponent(div);
