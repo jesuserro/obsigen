8c09f7bbce172a6d35d28fa8ad07a2c7c7d0918b
Jesús Erro <jesus@centraldereservas.com>
2024-08-14 22:33:27 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarYearSelect.tsx
+++ src/core/notes/calendar/CalendarYearSelect.tsx
@@ -1,51 +1,13 @@
-import React from 'react';
 
 interface CalendarYearSelectProps {
   currentYear: number;
   onChange: (year: number) => void;
 }
 
-const generateYears = (): string[] => {
-  const years: string[] = [];
-
-  // Agregar años antes de Cristo
-  for (let i = 500; i >= 1; i--) {
-    years.push(`${i} AC`);
-  }
-
-  // Agregar años después de Cristo
-  for (let i = 1; i <= 2030; i++) {
-    years.push(`${i} DC`);
-  }
-
-  return years;
-};
-
 function CalendarYearSelect({ currentYear, onChange }: CalendarYearSelectProps): JSX.Element {
-  const years = generateYears();
-
-  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedValue = event.target.value;
-    let selectedYear: number;
-
-    if (selectedValue.includes('AC')) {
-      selectedYear = -parseInt(selectedValue.replace(' AC', ''), 10);
-    } else {
-      selectedYear = parseInt(selectedValue.replace(' DC', ''), 10);
-    }
-
-    onChange(selectedYear);
-  };
-
+  
   return (
     <>
-      <select id="obs-year-picker" value={currentYear} onChange={handleYearChange}>
-        {years.map((year) => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
       <input
         type="number"
         placeholder="Enter year"
