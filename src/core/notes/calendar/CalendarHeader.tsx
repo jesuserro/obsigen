c9c4ae4b19f29875fa7e0bdb7d64199be11d4ec4
Jesús Erro <jesus@centraldereservas.com>
2024-08-22 18:34:56 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarHeader.tsx
+++ src/core/notes/calendar/CalendarHeader.tsx
@@ -6,7 +6,7 @@ interface CalendarHeaderProps {
   onAddEvent: () => void;
   onYearChange: (selectedYear: number) => void;
   onBookClick: () => void;
-  isBibleView: boolean; // Nuevo prop para indicar la vista actual
+  isBibleView: boolean; // Prop para indicar la vista actual
 }
 
 const CalendarHeader: React.FC<CalendarHeaderProps> = ({
@@ -14,7 +14,7 @@ const CalendarHeader: React.FC<CalendarHeaderProps> = ({
   onAddEvent,
   onYearChange,
   onBookClick,
-  isBibleView, // Recibir el nuevo prop
+  isBibleView, // Recibir el prop
 }) => {
   return (
     <>
@@ -41,16 +41,21 @@ const CalendarHeader: React.FC<CalendarHeaderProps> = ({
               className="bi bi-book-fill"
               viewBox="0 0 16 16"
             >
-              <path d="M0 1.5A.5.5 0 0 1 .5 1h7a.5.5 0 0 1 0 1H1v12h6.5a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5v-13z"/>
+              <path d="M0 1.5A.5.5 0 0 1 .5 1h7a.5.5 0 0 1 0 1H1v12h6.5a.5.5 0 0 1 0 1h-7a.5.5.5 0 0 1-.5-.5v-13z"/>
               <path d="M2 2v12h6.5a.5.5 0 0 0 .5-.5v-11a.5.5 0 0 0-.5-.5H2z"/>
               <path d="M10.5 1h-1v14h1a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2z"/>
             </svg>
           )}
         </button>
-        <button onClick={onAddEvent}>+</button>
-        <div className="year-selector-container">
-          <CalendarYearSelect currentYear={currentYear} onChange={onYearChange} />
-        </div>
+        {/* Ocultar el botón de añadir evento y el selector de años si isBibleView es true */}
+        {!isBibleView && (
+          <>
+            <button onClick={onAddEvent}>+</button>
+            <div className="year-selector-container">
+              <CalendarYearSelect currentYear={currentYear} onChange={onYearChange} />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
