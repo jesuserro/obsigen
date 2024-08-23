b43b4f5139fa49f521d92e67838463a97d062b4f
Jesús Erro <jesus@centraldereservas.com>
2024-08-23 20:57:37 +0200

    chore: commit save point

--- src/core/notes/bible/Bible.ts
+++ src/core/notes/bible/Bible.ts
@@ -3,10 +3,10 @@ import { App, MetadataCache, TFile } from 'obsidian';
 interface Note {
     title: string;
     date?: string;
+    verseStart?: number;  // Agregar campo para el versículo inicial
 }
 
 export function getChapterNotes(app: App, metadataCache: MetadataCache, files: TFile[] | undefined, chapterNumber: number): Note[] {
-    
     if (!files || files.length === 0) {
         return [];
     }
@@ -18,10 +18,16 @@ export function getChapterNotes(app: App, metadataCache: MetadataCache, files: T
 
     chapterFiles.forEach(file => {
         const fileName = file.name.replace(/\.md$/, '');
+
+        // Extraer el versículo inicial del nombre del archivo
+        const verseMatch = fileName.match(/Jn-\d{2}_(\d{2})/);
+        const verseStart = verseMatch ? parseInt(verseMatch[1], 10) : null;
+
         const noteTitle = fileName.replace(/^Jn-\d{2}_\d{2} - /, '');
 
         const note: Note = {
             title: noteTitle,
+            verseStart: verseStart || undefined, // Asignar versículo inicial si está disponible
         };
 
         chapterNotes.push(note);
@@ -29,4 +35,3 @@ export function getChapterNotes(app: App, metadataCache: MetadataCache, files: T
 
     return chapterNotes;
 }
-
--- src/core/notes/bible/BibleView.tsx
+++ src/core/notes/bible/BibleView.tsx
@@ -1,6 +1,6 @@
 import { App, MetadataCache, TFile } from 'obsidian';
 import React, { useEffect, useState } from 'react';
-import { getChapterNotes } from './Bible'; // Asegúrate de importar la función correctamente
+import { getChapterNotes } from './Bible';
 import { bibleStructure } from './bibleStructure';
 
 interface Props {
@@ -10,80 +10,79 @@ interface Props {
 }
 
 const BibleView: React.FC<Props> = ({ app, metadataCache, files }) => {
-  const [bibleNotes, setBibleNotes] = useState(bibleStructure);
+    const [bibleNotes, setBibleNotes] = useState(bibleStructure);
 
-  useEffect(() => {
-    // Ejecutar el efecto solo si todos los datos están disponibles
-    if (app && metadataCache && files && files.length > 0) {
-      
-  
-      const updatedBibleStructure = { ...bibleNotes };
-  
-      Object.entries(updatedBibleStructure["San Juan"].chapters).forEach(([chapterNumber, chapterInfo]) => {
-        const notes = getChapterNotes(app, metadataCache, files, parseInt(chapterNumber));
-  
-        // console.log(`Notes for chapter ${chapterNumber}:`, notes);
-  
-        // Asignar notas a las perícopas correspondientes
-        chapterInfo.pericopes.forEach(pericope => {
-          pericope.notes = notes.filter(note => true);
+    useEffect(() => {
+        if (!app || !metadataCache || !files || files.length === 0) {
+            return;
+        }
+
+        const updatedBibleStructure = { ...bibleNotes };
+
+        Object.entries(updatedBibleStructure["San Juan"].chapters).forEach(([chapterNumber, chapterInfo]) => {
+            const notes = getChapterNotes(app, metadataCache, files, parseInt(chapterNumber));
+
+            chapterInfo.pericopes.forEach(pericope => {
+                // Filtrar las notas que corresponden al rango de versículos de la perícopa
+                const notesForPericope = notes.filter(note => {
+                    return note.verseStart !== undefined && 
+                           note.verseStart >= pericope.verseRange[0] && 
+                           note.verseStart <= pericope.verseRange[1];
+                });
+                pericope.notes = notesForPericope;
+            });
         });
-      });
-  
-      setBibleNotes(updatedBibleStructure);
-    } else {
-    //   console.log('Waiting for app, metadataCache, or files to be available');
-    }
-  }, [app, metadataCache, files]);
-  
 
-  return (
-    <div className="bible-view-container">
-      <div className="books-grid">
-        {Object.entries(bibleNotes).map(([book, data]) => (
-          <div key={book} className="book-container">
-            <h2>{book} ({data.chapterCount})</h2>
-            <div className="chapters-grid">
-              {Object.entries(data.chapters).map(([chapterNumber, chapterInfo]) => (
-                <div key={chapterNumber} className="chapter-container">
-                  <h3>
-                    {chapterNumber}
-                    <sub>{chapterInfo.verseCount}</sub> {chapterInfo.title}
-                  </h3>
-                  <div className="pericopes-container">
-                    {chapterInfo.pericopes.length > 0 ? (
-                      chapterInfo.pericopes.map((pericope, index) => (
-                        <div key={index} className="pericope-container">
-                          <h4>
-                            {pericope.title} ({pericope.verseRange[0]}-{pericope.verseRange[1]})
-                          </h4>
-                          <div className="events-container">
-                            {pericope.notes.length > 0 ? (
-                              pericope.notes.map((note, noteIndex) => (
-                                <div
-                                  key={noteIndex}
-                                  className="event-icon"
-                                  title={note.title}
-                                />
-                              ))
-                            ) : (
-                              <span>Sin notas</span>
-                            )}
-                          </div>
+        setBibleNotes(updatedBibleStructure);
+    }, [app, metadataCache, files]);
+
+    return (
+        <div className="bible-view-container">
+            <div className="books-grid">
+                {Object.entries(bibleNotes).map(([book, data]) => (
+                    <div key={book} className="book-container">
+                        <h2>{book} ({data.chapterCount})</h2>
+                        <div className="chapters-grid">
+                            {Object.entries(data.chapters).map(([chapterNumber, chapterInfo]) => (
+                                <div key={chapterNumber} className="chapter-container">
+                                    <h3>
+                                        {chapterNumber}
+                                        <sub>{chapterInfo.verseCount}</sub> {chapterInfo.title}
+                                    </h3>
+                                    <div className="pericopes-container">
+                                        {chapterInfo.pericopes.length > 0 ? (
+                                            chapterInfo.pericopes.map((pericope, index) => (
+                                                <div key={index} className="pericope-container">
+                                                    <h4>
+                                                        {pericope.title} ({pericope.verseRange[0]}-{pericope.verseRange[1]})
+                                                    </h4>
+                                                    <div className="events-container">
+                                                        {pericope.notes.length > 0 ? (
+                                                            pericope.notes.map((note, noteIndex) => (
+                                                                <div
+                                                                    key={noteIndex}
+                                                                    className="event-icon"
+                                                                    title={note.title}
+                                                                />
+                                                            ))
+                                                        ) : (
+                                                            <span>Sin notas</span>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            ))
+                                        ) : (
+                                            <span>Sin perícopas</span>
+                                        )}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                      ))
-                    ) : (
-                      <span>Sin perícopas</span>
-                    )}
-                  </div>
-                </div>
-              ))}
+                    </div>
+                ))}
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default BibleView;
