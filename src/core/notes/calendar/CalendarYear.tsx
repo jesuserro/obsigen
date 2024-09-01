a1d196b802bec97a057040516d9fd696a0b8e259
Jesús Erro <jesus@centraldereservas.com>
2024-09-01 13:05:04 +0200

    chore: commit save point

--- src/core/notes/calendar/CalendarYear.tsx
+++ src/core/notes/calendar/CalendarYear.tsx
@@ -1,4 +1,4 @@
-import MonthUI from './MonthUI'; // Importar el nuevo componente MonthUI
+import MonthUI from './MonthUI';
 
 interface CalendarYearProps {
   year: number;
@@ -11,7 +11,7 @@ function CalendarYear({ year }: CalendarYearProps): JSX.Element {
     const monthKey = `${year}-${(month).toString().padStart(2, '0')}`;
     
     return (
-      <MonthUI key={monthKey} year={year} month={month} /> // Usar MonthUI en lugar de CalendarMonth
+      <MonthUI key={monthKey} year={year} month={month} /> 
     );
   });
 
