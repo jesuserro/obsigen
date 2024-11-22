1a920807526ec12e96b73d0b587d6b7caa00031a
Jesús Erro <olagato@gmail.com>
2024-11-22 20:03:37 +0100

    chore: commit save point

--- src/core/notes/bible/BibleViewStructure.ts
+++ src/core/notes/bible/BibleViewStructure.ts
@@ -1,7 +1,9 @@
+import Sabiduria from "./books/Sabiduría";
 import SanJuan from "./books/SanJuan";
 import SanLucas from "./books/SanLucas";
 import SanMarcos from "./books/SanMarcos";
 import SanMateo from "./books/SanMateo";
+import Santiago from "./books/Santiago";
 export interface BibleImage {
 	type: "local" | "url"; // 'local' para imágenes guardadas en el vault, 'url' para imágenes online
 	path: string; // Ruta del archivo en el vault o URL de la imagen
@@ -24,8 +26,10 @@ export interface BookStructure {
 }
 
 export const bibleStructure: { [book: string]: BookStructure } = {
+	"Sabiduria": Sabiduria,
 	"San Mateo": SanMateo,
 	"San Marcos": SanMarcos,
 	"San Lucas": SanLucas,
 	"San Juan": SanJuan,
+	"Santiago": Santiago,
 };
---
+++ src/core/notes/bible/books/Santiago.ts
@@ -0,0 +1,29 @@
+import { BookStructure } from "../BibleViewStructure";
+
+const Santiago: BookStructure = {
+    chapters: {
+        1: {
+            title: "Pruebas, sabiduría y fe verdadera",
+            pericopes: []
+        },
+        2: {
+            title: "Fe y obras, y el trato sin favoritismos",
+            pericopes: []
+        },
+        3: {
+            title: "El poder de la lengua y la sabiduría divina",
+            pericopes: []
+        },
+        4: {
+            title: "La soberbia, la humildad y el juicio hacia los demás",
+            pericopes: []
+        },
+        5: {
+            title: "Advertencias a los ricos y la paciencia en el sufrimiento",
+            pericopes: []
+        }
+    }
+
+};
+
+export default Santiago;
