f09f1a4a7f17406d82774f3f53c5e863872a3604
Jesús Erro <olagato@gmail.com>
2024-11-28 19:09:42 +0100

    chore: commit save point

---
+++ src/core/notes/bible/books/2Samuel.ts
@@ -0,0 +1,114 @@
+import { BookStructure } from "../BibleViewStructure";
+
+const SegundaSamuel: BookStructure = {
+	chapters: {
+		1: {
+			title: "David lamenta la muerte de Saúl y Jonatán",
+			pericopes: []
+		},
+		2: {
+			title: "David es ungido rey de Judá",
+			pericopes: []
+		},
+		3: {
+			title: "Abner se une a David",
+			pericopes: []
+		},
+		4: {
+			title: "La muerte de Isboset",
+			pericopes: []
+		},
+		5: {
+			title: "David es ungido rey de Israel",
+			pericopes: []
+		},
+		6: {
+			title: "El arca de Dios es llevada a Jerusalén",
+			pericopes: []
+		},
+		7: {
+			title: "El pacto de Dios con David",
+			pericopes: [{
+                title: "Pacto nº5 Davídico - Profecía dinástica de Natán",
+                verseRange: [1, 17],
+                images: [
+                    {
+                        type: "local",
+                        path: "DALL·E 2024-11-28 19.07.33 - Davidic Covenant number 5 - 2 S 7 1-17.webp",
+                        altText: "DALL·E 2024-11-28 19.07.33 - Davidic Covenant number 5 - 2 S 7 1-17",
+                    },
+                ],
+            }]
+		},
+		8: {
+			title: "Las victorias de David",
+			pericopes: []
+		},
+		9: {
+			title: "David muestra bondad a Mefiboset",
+			pericopes: []
+		},
+		10: {
+			title: "David derrota a los amonitas y sirios",
+			pericopes: []
+		},
+		11: {
+			title: "El pecado de David con Betsabé",
+			pericopes: []
+		},
+		12: {
+			title: "El arrepentimiento de David",
+			pericopes: []
+		},
+		13: {
+			title: "Amnón y Tamar",
+			pericopes: []
+		},
+		14: {
+			title: "Absalón regresa a Jerusalén",
+			pericopes: []
+		},
+		15: {
+			title: "Absalón conspira contra David",
+			pericopes: []
+		},
+		16: {
+			title: "David huye de Jerusalén",
+			pericopes: []
+		},
+		17: {
+			title: "El consejo de Ahitofel y Husai",
+			pericopes: []
+		},
+		18: {
+			title: "La muerte de Absalón",
+			pericopes: []
+		},
+		19: {
+			title: "El regreso de David a Jerusalén",
+			pericopes: []
+		},
+		20: {
+			title: "La rebelión de Seba",
+			pericopes: []
+		},
+		21: {
+			title: "La venganza de los gabaonitas",
+			pericopes: []
+		},
+		22: {
+			title: "El cántico de acción de gracias de David",
+			pericopes: []
+		},
+		23: {
+			title: "Las últimas palabras de David y sus valientes",
+			pericopes: []
+		},
+		24: {
+			title: "El censo de David y la peste",
+			pericopes: []
+		},
+	},
+};
+
+export default SegundaSamuel;
