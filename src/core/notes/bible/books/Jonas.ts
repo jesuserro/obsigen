90ffd665c46b47ee551adae9ef1d80ee9c747408
Jesús Erro <olagato@gmail.com>
2024-12-03 13:04:52 +0100

    chore: commit save point

--- src/core/notes/bible/books/Jonas.ts
+++ src/core/notes/bible/books/Jonas.ts
@@ -8,7 +8,17 @@ const Jonas: BookStructure = {
 		},
 		2: {
 			title: "La oración de Jonás desde el pez",
-			pericopes: []
+			pericopes: [{
+                title: "La oración de Jonás desde el pez",
+                verseRange: [1, 11],
+                images: [
+                    {
+                        type: "local",
+                        path: "DALL·E 2024-12-03 13.02.47 - Jon 2 1-11 - Jonah praying inside the belly of a giant fish.webp",
+                        altText: "DALL·E 2024-12-03 13.02.47 - Jon 2 1-11 - Jonah praying inside the belly of a giant fish",
+                    },
+                ],
+            }]
 		},
 		3: {
 			title: "Jonás predica en Nínive",
