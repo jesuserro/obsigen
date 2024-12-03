aa2f89cd487fa1253d2121b4cd8945536edb928f
Jesús Erro <olagato@gmail.com>
2024-12-03 13:38:35 +0100

    chore: commit save point

--- src/core/notes/bible/books/Galatas.ts
+++ src/core/notes/bible/books/Galatas.ts
@@ -6,12 +6,22 @@ const Galatas: BookStructure = {
 			title: "No hay otro evangelio",
 			pericopes: [{
                 title: "",
-                verseRange: [1, 11],
+                verseRange: [1, 10],
                 images: [
                     {
                         type: "local",
                         path: "DALL·E 2024-12-03 13.21.26 - Gálatas 1 6-10 - Un único evangelio o anatema.webp",
                         altText: "DALL·E 2024-12-03 13.21.26 - Gálatas 1 6-10 - Un único evangelio o anatema",
+                    },
+                ],
+            }, {
+                title: "",
+                verseRange: [11, 24],
+                images: [
+                    {
+                        type: "local",
+                        path: "DALL·E 2024-12-03 13.35.15 - Saint Paul's divine calling based on Galatians 1_11-17.webp",
+                        altText: "DALL·E 2024-12-03 13.35.15 - Saint Paul's divine calling based on Galatians 1_11-17",
                     },
                 ],
             }]
