08104afa0141038221808b8288b80830e6e79575
Jesús Erro <olagato@gmail.com>
2024-12-11 13:11:21 +0100

    chore: commit save point

--- src/core/notes/bible/BibleViewChaptersUI.tsx
+++ src/core/notes/bible/BibleViewChaptersUI.tsx
@@ -108,7 +108,7 @@ const BibleChaptersView: React.FC<Props> = ({ app, bookRefs, selectedBook, setSe
                                                         </div>
                                                         {image.coordinates && (
                                                             <div className="map-overlay" onClick={(e) => e.stopPropagation()}>
-                                                                <a href={`obsidian://mapview?do=open&centerLat=${image.coordinates[0]}&centerLng=${image.coordinates[1]}&chosenMapSource=0&linkColor=red&mapZoom=13&name=Default&query=&showLinks=false`} target="_blank" title={image.locations ? image.locations[0].replace(/\[\[|\]\]/g, '') : ''}>
+                                                                <a href={`obsidian://mapview?do=open&centerLat=${image.coordinates[0]}&centerLng=${image.coordinates[1]}&chosenMapSource=0&linkColor=red&mapZoom=13&name=Default&query=&showLinks=true&fitBounds=true&showName=true&showPins=true&pinColor=red`} target="_blank" title={image.locations ? image.locations[0].replace(/\[\[|\]\]/g, '') : ''}>
                                                                     <FaMapMarkerAlt /> {/* Usamos el icono de marcador */}
                                                                 </a>
                                                             </div>
