b7a78d5b677c454783c9edfa5e78618e7560ea07
Jesús Erro <olagato@gmail.com>
2024-12-12 19:13:27 +0100

    chore: commit save point

--- src/core/notes/timeline/TimelineUI.tsx
+++ src/core/notes/timeline/TimelineUI.tsx
@@ -2,6 +2,7 @@ import { App } from 'obsidian';
 import React, { useEffect, useState } from 'react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { ChapterImage, fetchChapterImages } from './Timeline';
 
 interface Props {
@@ -24,7 +25,7 @@ const TimelineView: React.FC<Props> = ({ app, bookRefs, selectedBook, setSelecte
 
     return (
         <div className="timeline-view">
-            <VerticalTimeline>
+            <VerticalTimeline layout="1-column-left">
                 {Object.entries(chapterImages).map(([key, images]) => (
                     images.map((image, index) => (
                         <VerticalTimelineElement
@@ -32,19 +33,25 @@ const TimelineView: React.FC<Props> = ({ app, bookRefs, selectedBook, setSelecte
                             date={image.date ? new Date(image.date).toLocaleDateString() : 'Unknown date'}
                             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
                             icon={<FaMapMarkerAlt />}
+                            contentStyle={{ background: '#1e1e1e', color: '#e0e0e0' }}
+                            contentArrowStyle={{ borderRight: '7px solid  #1e1e1e' }}
                         >
                             <div className="timeline-element-content">
-                                <img src={image.path} alt={image.alt} className="timeline-image" />
-                                <h3 className="vertical-timeline-element-title">{image.title}</h3>
-                                <h4 className="vertical-timeline-element-subtitle">{image.pericopeTitle}</h4>
-                                <p>{image.versePassage}</p>
-                                {image.locations && image.locations.length > 0 && image.coordinates && (
-                                    <div className="map-overlay">
-                                        <a href={`obsidian://mapview?do=open&centerLat=${image.coordinates[0]}&centerLng=${image.coordinates[1]}&chosenMapSource=0&linkColor=red&mapZoom=13&name=Default&query=&showLinks=true&fitBounds=true&showName=true&showPins=true&pinColor=red`} target="_blank" title={image.locations[0].replace(/\[\[|\]\]/g, '')}>
-                                            <FaMapMarkerAlt /> {image.locations[0].replace(/\[\[|\]\]/g, '')}
-                                        </a>
-                                    </div>
-                                )}
+                                <div className="timeline-image-container">
+                                    <img src={image.path} alt={image.alt} className="timeline-image" />
+                                </div>
+                                <div className="timeline-text-container">
+                                    <h3 className="vertical-timeline-element-title">{image.title}</h3>
+                                    <h4 className="vertical-timeline-element-subtitle">{image.pericopeTitle}</h4>
+                                    <p>{image.versePassage}</p>
+                                    {image.locations && image.locations.length > 0 && image.coordinates && (
+                                        <div className="map-overlay">
+                                            <a href={`obsidian://mapview?do=open&centerLat=${image.coordinates[0]}&centerLng=${image.coordinates[1]}&chosenMapSource=0&linkColor=red&mapZoom=13&name=Default&query`}>
+                                                <FaMapMarkerAlt /> View on Map
+                                            </a>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         </VerticalTimelineElement>
                     ))
